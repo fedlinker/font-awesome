91f1726423018a58c770602b923a75b296da44db
沈经纬 <327538014@qq.com>
2020-01-15 16:07:59 +0800

    fix(tools): fixed the error of svg filename

--- tools/parse-svg.js
+++ tools/parse-svg.js
@@ -1,7 +1,7 @@
-const svgr = require("@svgr/core");
-const fs = require("fs-extra");
-const path = require("path");
-const ora = require("ora");
+const svgr = require('@svgr/core');
+const fs = require('fs-extra');
+const path = require('path');
+const ora = require('ora');
 
 /**
  * 将单词首字母大写
@@ -17,22 +17,22 @@ const firstLetterToUpperCase = letter => {
  * @param {*} name
  * @param {*} prefix
  */
-const parseFileName = (name, prefix = "") => {
-  const nameSplit = name.split("-");
+const parseFileName = (name, prefix = '') => {
+  const nameSplit = name.split('-');
   return `${firstLetterToUpperCase(prefix)}${nameSplit
     .map(n => firstLetterToUpperCase(n))
-    .join("")}`;
+    .join('')}`;
 };
 
 (async function() {
-  const svgPath = path.resolve(__dirname, "../src/svg");
+  const svgPath = path.resolve(__dirname, '../src/svg');
   const basePath = path.resolve(
     __dirname,
-    "../node_modules/@fortawesome/fontawesome-free/svgs"
+    '../node_modules/@fortawesome/fontawesome-free/svgs'
   );
   const dirs = await fs.readdir(basePath);
 
-  console.log("The svg files are parsing, please wait! 🚀");
+  console.log('The svg files are parsing, please wait! 🚀');
   const spinner = ora().start();
 
   for (let dir of dirs) {
@@ -40,30 +40,30 @@ const parseFileName = (name, prefix = "") => {
     const stat = await fs.stat(p);
     if (stat.isDirectory()) {
       const files = await fs.readdir(p);
-      const svg = await fs.readFile(
-        path.resolve(basePath, dir, files[0]),
-        "utf-8"
-      );
 
       for (let fileName of files) {
-        const componentName = parseFileName(fileName.split(".svg")[0], dir);
-        const newFileName = componentName + ".tsx";
+        const svg = await fs.readFile(
+          path.resolve(basePath, dir, fileName),
+          'utf-8'
+        );
+        const componentName = parseFileName(fileName.split('.svg')[0], dir);
+        const newFileName = componentName + '.tsx';
         spinner.start(`${dir}/${fileName}`);
         const jsCode = await svgr.default(
           svg,
           {
             icon: true,
             plugins: [
-              "@svgr/plugin-svgo",
-              "@svgr/plugin-jsx",
-              "@svgr/plugin-prettier",
+              '@svgr/plugin-svgo',
+              '@svgr/plugin-jsx',
+              '@svgr/plugin-prettier',
             ],
             template(
               { template },
               opts,
               { imports, componentName, props, jsx, exports }
             ) {
-              const typeScriptTpl = template.smart({ plugins: ["typescript"] });
+              const typeScriptTpl = template.smart({ plugins: ['typescript'] });
               return typeScriptTpl.ast`
               import React, { SVGProps } from "react";
               import generateIcon from "../../generateIcon";
@@ -78,10 +78,10 @@ const parseFileName = (name, prefix = "") => {
         );
 
         await fs.outputFile(path.resolve(svgPath, dir, newFileName), jsCode, {
-          encoding: "utf-8",
+          encoding: 'utf-8',
         });
       }
     }
   }
-  spinner.succeed("Congratulations, parsing success!");
+  spinner.succeed('Congratulations, parsing success!');
 })();
