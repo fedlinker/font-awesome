13dca95f608d8e3dd3fdefe70d8de1682316049d
沈经纬 <327538014@qq.com>
2020-01-16 14:35:30 +0800

    fix(generateIcon): adjust display property

--- src/generateIcon.tsx
+++ src/generateIcon.tsx
@@ -1,4 +1,4 @@
-import React, { SFC, SVGProps, PropsWithChildren, HTMLAttributes } from "react";
+import React, { SFC, SVGProps, PropsWithChildren, HTMLAttributes } from 'react';
 
 type IIconComponent = SFC<SVGProps<SVGSVGElement>>;
 export interface IIconBaseProps
@@ -6,18 +6,19 @@ export interface IIconBaseProps
 export type IIconComponentType = SFC<IIconBaseProps>;
 
 function IconBase(props: IIconBaseProps) {
-  const { children, ...rest } = props;
-  return <span {...rest}>{children}</span>;
+  const { children, style, ...rest } = props;
+  return (
+    <span style={{ display: 'inline-flex', ...style }} {...rest}>
+      {children}
+    </span>
+  );
 }
 
 export default function generateIcon(IconComponent: IIconComponent) {
   const Icon: IIconComponentType = (props: IIconBaseProps) => {
     return (
       <IconBase {...props}>
-        <IconComponent
-          fill="currentColor"
-          style={{ verticalAlign: "middle" }}
-        />
+        <IconComponent fill="currentColor" />
       </IconBase>
     );
   };
